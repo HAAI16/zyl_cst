6f5ad181081ca2dc5705b4c163439a8a5cb41397
haai16 <2920485408@qq.com>
2019-12-17 19:57:26 +0800

    A Demo Project

---
+++ src/main/java/com/hpu/ZylApplication.java
@@ -0,0 +1,13 @@
+package com.hpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZylApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZylApplication.class, args);
+    }
+
+}
+
